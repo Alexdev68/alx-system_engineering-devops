7bc619b9d5118b8238d12b1aa081dc5b3da6807b
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-27 07:37:29 -0700

    Fixing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@ include stdlib
 
 file_line { 'Turn off passwd auth':
 path  => '/etc/ssh/ssh_config',
-line  => '	PasswordAuthentication off',
+line  => '	PasswordAuthentication on',
 match => '^	PasswordAuthentication',
 }
 
