a69ed8bc818f3608ec7b3739639cda92b53f6d1b
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-27 06:58:59 -0700

    Changed the file path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # This script uses puppet to make changes to our configuration file
 
-file { '/root/.ssh/config':
+file { '/etc/ssh/config':
 ensure  => present,
 content => @(END);
 Host 303903-web-01
 	HostName 52.91.126.140
 	User ubuntu
-	IdentityFile /root/.ssh/school
+	IdentityFile /etc/ssh/school
 	PasswordAuthentication no
 	ChallengeResponseAuthentication no
 END
