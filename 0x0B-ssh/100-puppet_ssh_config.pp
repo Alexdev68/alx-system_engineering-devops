f5d974ad5db4d87ddebeece1670eef14db633c83
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-27 06:25:23 -0700

    New file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# This script uses puppet to make changes to our configuration file
+
+file { '/root/.ssh/config':
+ensure  => present,
+content => @(END),
+Host 303903-web-01
+	HostName 52.91.126.140
+	User ubuntu
+	IdentityFile ~/.ssh/school
+END
+}
