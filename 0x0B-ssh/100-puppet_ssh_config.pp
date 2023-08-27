30cdee30f3c0310698282eb2f16e9340cd5a44a5
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-27 07:25:07 -0700

    Fixing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # This script uses puppet to make changes to our configuration file
+include stdlib
 
-file { '/etc/ssh/config':
-ensure  => present,
-content => @(END);
-Host 303903-web-01
-	HostName 52.91.126.140
-	User ubuntu
-	IdentityFile /etc/ssh/school
-	PasswordAuthentication no
-	ChallengeResponseAuthentication no
-END
+file_line { 'Turn off passwd auth':
+path  => '/etc/ssh/ssh_config',
+line  => 'PasswordAuthentication off',
+match => '^PasswordAuthentication',
+}
+
+file_line { 'IdentityFile':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile /etc/ssh/school'
 }
